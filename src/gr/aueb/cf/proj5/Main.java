827b4854e931819e07a6421238afa64fa09fe931
tvaiopoulos <tvaiopoulos@gmail.com>
2023-12-11 20:26:24 +0200

    sayHello method

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Coding Factory!!!!!!");
     }
+
+    public static void sayHello() {
+        System.out.println("Hello World!!!");
+    }
 }
